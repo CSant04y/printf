7473beab70583d28eec50f041a4af39abffe198f
aydentownsley <aydentownsley@gmail.com>
2020-10-24 15:54:31 +0000

    adding _printf & holberton setup

---
+++ _printf.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * _printf - prints a range of arguments it is given, given a format specifier
+ *
+ * @format: a character string comprised of zero or more directives
+ *
+ * Return: number of chararcters printed (minus null byte)
+ */
+
+int _printf(const char *format, ...)
+{
+	
+}
---
+++ holberton.h
@@ -0,0 +1,10 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+/* HEADERS */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdargs.h>
+
+#endif /* _HOLBERTON_H_ */
