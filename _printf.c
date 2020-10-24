dc2363e5adcd5cde1a09bb7349a6651d6c5c6e67
CSant04y <esquivelcarlos27893@gmail.com>
2020-10-24 19:43:09 +0000

    set up the basic skelton of printf

--- _printf.c
+++ _printf.c
@@ -10,5 +10,30 @@
 
 int _printf(const char *format, ...)
 {
-	
+	unsigned int len = 0;
+	unsigned int itr;
+	va_list ls;
+	int (*f)(va_list ls);
+
+	va_start(ls, format);
+	/** We are checking to see if format is NULL as well as if fomat of itr
+	 *  is equalto the null terminating byte */
+	for (itr = 0; format != NULL && format[itr] != '\0'; itr++)
+	{
+		if (format[itr] != '%')
+		{
+			_putchar(format[itr]);
+			len++;
+		}
+		if (format[itr] == '%' && *(format + 1) == '%')
+		{
+				_putchar('%');
+				len++;
+		}
+		else
+		{
+			f = func_select(va_list ls);
+		}
+	}
+
 }
