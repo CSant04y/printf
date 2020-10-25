30e254493bf1475e1304f61dd79546b5bc1bee18
CSant04y <esquivelcarlos27893@gmail.com>
2020-10-25 18:38:18 +0000

    added selct func with integers

--- selected_func.c
+++ selected_func.c
@@ -29,7 +29,7 @@ int op_str(va_list ls)
 
 /**
  * op_char - prints characters
- *
+ * ls: name of va_list
  * Return: int of printed lenght
  * OR NULL if ptr is NULL, exit 101
  */
@@ -47,4 +47,40 @@ int op_char(va_list ls)
 
 	_putchar(*ptr);
 	return (1);
+}
+/**
+ * op_num - This prints a integer and decimal integer to the standard output
+ * @ls: name of va_list
+ * Return: length
+ */
+int op_num(va_list ls)
+{
+	int num = va_arg(ls, int);
+
+		if (num < 0)
+		{
+			_putchar('-');
+			num *= -1;
+		}
+		else if (num / 10)
+			r_len(num);
+	return (num);
+}
+/**
+ * r_len - this gets the lenght of a sting using recursion
+ * @n: passing in integer
+ *
+ */
+int r_len(int n)
+{
+	int i, len;
+
+	printf("%d\n", n);
+	if (n == 0)
+		return (0);
+	i = n % 10;
+	len = 1 + r_len(n / 10);
+	_putchar(i + '0');
+
+	return (len);
 }
