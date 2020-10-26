b7d818951c704157226827018d4a4ef2821edeb1
CSant04y <esquivelcarlos27893@gmail.com>
2020-10-26 17:43:39 +0000

    added update

---
+++ main.c
@@ -0,0 +1,32 @@
+#include <limits.h>
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int len;
+	int len2;
+	int k = 54;
+	len = _printf("Let's try to printf a simple sentence.\n");
+	len2 = printf("Let's try to printf a simple sentence.\n");
+	_printf("Character:[%c]\n", 'g');
+	_printf("Length:[%d, %i]\n", len, len);
+	printf("Length:[%d, %i]\n", len2, len2);
+	_printf("Character:[%c]\n", 'H');
+	printf("Character:[%c]\n", 'H');
+	_printf("String:[%s]\n", "I am a string !");
+	printf("String:[%s]\n", "I am a string !");
+	len = _printf("Percent:[%%]\n");
+	len2 = printf("Percent:[%%]\n");
+	_printf("Len:[%d]\n", len);
+	printf("Len:[%d]\n", len2);
+	_printf("[%d]\n", -123454);
+	_printf("[%d, %d]\n", 123, k);
+	_printf("[%d]\n", 450);
+	return (0);
+}
