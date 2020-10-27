3ebf77d2ab1076959c627b068dcb2c2038b851b5
CSant04y <esquivelcarlos27893@gmail.com>
2020-10-27 20:49:01 +0000

    made file to make int to bin

---
+++ binary.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+/**
+ * itob - this makes int into binary
+ * @ls: va_list
+ * Return: 1 if passed -1 for fail
+ */
+int itob(va_list ls)
+{
+	int len = 0;
+	int num = va_arg(ls, int);
+
+	len = ihelp(num);
+	return (len);
+}
+/**
+ * ihelp - recursive function that returns the length of the binary
+ * as well as dividing n by 2 to get either 1 or 0
+ * @n: number being passed in from itob
+ * Return: is len
+ */
+int ihelp(int n)
+{
+	int len = 0;
+
+	if (!n)
+		return (-1);
+
+	if (n >= 1)
+	{
+		if (n > 1)
+			len = 1 + ihelp(n / 2);
+		_putchar((n % 2) + '0');
+	}
+	return (len);
+}
