72d37966d6e5d518aa8cd4be8dd386b7636c25b7
CSant04y <esquivelcarlos27893@gmail.com>
2020-10-28 18:20:21 +0000

    adding new version of string reverse that does not malloc!

--- revstr.c
+++ revstr.c
@@ -11,8 +11,7 @@
 int revstr(va_list ls)
 {
 	char *s = va_arg(ls, char *);
-	int i = 0, count = 0, itr2 = 0, itr;
-	char *store;
+	int i = 0, count = 0;
 
 	if (s == NULL)
 	{
@@ -22,27 +21,12 @@ int revstr(va_list ls)
 			_putchar(*s);
 			s++;
 		}
-		return (-1);
+		return (6);
 	}
 	for (; s[i] != '\0'; i++)
-		count++;
-	store = malloc((sizeof(char) * count) + 1);
-	if (!store)
-	{
-		free(store);
-		return (-1);
-	}
+		;
+	for (i--; i >= 0; i--)
+		count += _putchar(s[i]);
 
-	for (itr = count; itr >= 0; itr--, itr2++)
-	{
-		store[itr2] = s[itr];
-	}
-
-	store[itr2 + 1] = '\0';
-
-	for (itr = 0; itr <= count; itr++)
-		_putchar(store[itr]);
-
-	free(store);
 	return (count);
 }
