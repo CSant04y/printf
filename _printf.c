b9326f7862c57565d36cd4f0f4429172a4a77e76
aydentownsley <aydentownsley@gmail.com>
2020-10-28 01:43:13 +0000

    trying to reset

--- _printf.c
+++ _printf.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
- * _printf - prints a range of arguments it is given, given a format specifier
+ * _printf - prints a range of arguments it is given, given a format specifier.
  *
  * @format: a character string comprised of zero or more directives
  *
